19fb455fb6b7962f9949f231d9a3c82d2257b84b
Kyle John <njk230@gmail.com>
2017-03-24 11:31:59 +0900

    Add android project

---
+++ Android/Equalizen/Equalizen/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Equalizen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var source = new HtmlWebViewSource();
+            source.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
+
+            //var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            //Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.PCLTextResource.txt");
+            //string text = "";
+            //using (var reader = new System.IO.StreamReader(stream))
+            //{
+            //    text = reader.ReadToEnd();
+            //}
+
+            source.Html = @"
+<html>
+<head>
+
+</head/>
+<body>
+<p><a href=""index.html"">next page</a></p>
+</body/>
+</html/>
+";
+
+            Browser.Source = source;
+        }
+    }
+}
