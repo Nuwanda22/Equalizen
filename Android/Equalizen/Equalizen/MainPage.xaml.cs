e044bd1c0c0890dff9821982eeba72e8c3f7b103
Kyle John <njk230@gmail.com>
2017-03-24 11:42:02 +0900

    Reset project

--- Android/Equalizen/Equalizen/MainPage.xaml.cs
+++ Android/Equalizen/Equalizen/MainPage.xaml.cs
@@ -14,30 +14,6 @@ namespace Equalizen
         public MainPage()
         {
             InitializeComponent();
-
-            var source = new HtmlWebViewSource();
-            source.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
-
-            //var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            //Stream stream = assembly.GetManifestResourceStream("WorkingWithFiles.PCLTextResource.txt");
-            //string text = "";
-            //using (var reader = new System.IO.StreamReader(stream))
-            //{
-            //    text = reader.ReadToEnd();
-            //}
-
-            source.Html = @"
-<html>
-<head>
-
-</head/>
-<body>
-<p><a href=""index.html"">next page</a></p>
-</body/>
-</html/>
-";
-
-            Browser.Source = source;
         }
     }
 }
